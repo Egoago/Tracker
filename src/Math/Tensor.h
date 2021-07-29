4466e3582852a20da7c08930afb3b8fbda5215bb
Egoago <61714141+Egoago@users.noreply.github.com>
2021-07-29 09:54:32 +0200

    tensor fixed

--- src/Math/Tensor.h
+++ src/Math/Tensor.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <initializer_list>
 #include <string>
+//TODO remove log
 #include "../Misc/Log.h"
 
 namespace tr {
@@ -9,92 +10,146 @@ namespace tr {
 	{
 		typedef unsigned int uint;
 
-		uint* dimensions = nullptr;
-		uint dimCount = 0;
+		uint* _shape = nullptr;
+		uint dims = 0u;
 		CellType* storage = nullptr;
 		bool allocated = false;
+		uint size = 0u;
 		void error(const std::string& msg) {
 			Logger::error(msg);
 			exit(1);
 		}
+
+		void calculateSize();
+		void deallocate();
 	public:
 		Tensor() {}
-		Tensor(std::initializer_list<uint> dimensions) { allocate(dimensions); }
-		~Tensor() {
-			if (allocated) {
-				delete[] storage;
-				delete dimensions;
-			}
-		}
-		void allocate(std::initializer_list<uint> dimensions) {
-			if (allocated) {
-				delete[] storage;
-				delete this->dimensions;
-			}
-			dimCount = (uint)dimensions.size();
-			if (dimCount == 0) return;
-				this->dimensions = new uint[dimCount];
-			for (const auto i : dimensions)
-				this->dimensions[i] = i;
-			allocated = true;
-			storage = new CellType[getSize()];
+		Tensor(std::initializer_list<uint> shape) { allocate(shape); }
+		Tensor(std::initializer_list<uint> shape, const CellType& value) {
+			allocate(shape);
+			std::fill_n(storage, size, value);
 		}
 
-		inline uint getDim() const { return dimCount; }
-		uint getSize() const {
-			if (!allocated) return 0;
-			uint size = 1;
-			for (uint i = 0; i < dimCount; i++)
-				size *= dimensions[i];
-			return size;
-		}
-		//Reinstantiates the Tensor with the current dimensions
-		void clear() {
-			allocate(dimensions);
-		}
+		~Tensor() { deallocate(); }
+
+		void allocate(std::initializer_list<uint> shape);
+
+		inline uint getDim() const { return dims; }
+		inline uint getSize() const { return size; }
 
 		//Access element with range check, slower
-		CellType& at(std::initializer_list<uint> indices) {
-			if (!allocated)
-				error("tensor not yet allocated");
-			if (indices.size() != dimCount)
-				error(to_string(indices.size())
-					+ " indices given for a tensor with dimensionality of "
-					+ to_string(dimCount));
-			uint i = 0;
-			for (const auto index : indices) {
-				i++;
-				if (index >= dimensions[i])
-					error("index"
-						+ to_string(index)
-						+ " at dimension "
-						+ to_string(i-1)
-						+ " is out of range of "
-						+ to_string(dimensions[i]));
-			}
-			return this->operator()(indices);
-		}
+		CellType& at(std::initializer_list<uint> indices);
 
 		//Access element without range check, faster
-		CellType& operator()(std::initializer_list<uint> indices) {
-			uint indexSum = 0;
-			uint i = 0;
-			for (const auto index : indices) {
-				indexSum *= dimensions[i];
-				indexSum += index;
-				i++;
-			}
-			return storage[indexSum];
-		}
+		CellType& operator()(std::initializer_list<uint> indices);
 
-		std::string toString() {
-			std::string str;
-			for (uint i = 0; i < getSize(); i++)
-				str += to_string(at({i})) + " ";
-			return str;
-		}
+		std::string toString();
 
 		inline CellType* begin() { return storage; }
-		inline CellType* end() { return storage + getSize(); }
-	};
+		inline CellType* end() { return storage + size; }
+};
+
+	template<typename CellType>
+	inline void Tensor<CellType>::calculateSize()
+	{
+		Logger::logProcess("calculating size");
+		if (!allocated || dims == 0u) size = 0u;
+		else {
+			size = 1u;
+			if (_shape == nullptr)
+				error("shape deleted");
+			Logger::log("size: " + to_string(size));
+			for (uint i = 0u; i < dims; i++) {
+				size = size * _shape[i];
+				Logger::log("dimension: " + to_string(_shape[i]));
+				Logger::log("size: " + to_string(size));
+			}
+		}
+		Logger::logProcess("calculating size");
+	}
+	template<typename CellType>
+	inline void Tensor<CellType>::deallocate()
+	{
+		Logger::logProcess("deallocating tensor");
+		if (storage != nullptr) {
+			delete[] storage;
+			storage = nullptr;
+		}
+		if (_shape != nullptr) {
+			delete[] _shape;
+			_shape = nullptr;
+		}
+		allocated = false;
+		Logger::logProcess("deallocating tensor");
+	}
+	template<typename CellType>
+	inline void Tensor<CellType>::allocate(std::initializer_list<uint> shape)
+	{
+		Logger::logProcess("allocating");
+		if (allocated)
+			deallocate();
+		dims = (uint)shape.size();
+		Logger::log("dims: " + to_string(dims));
+		if (dims == 0u) return;
+		this->_shape = new uint[dims];
+		int i = 0;
+		for (const auto dim : shape) {
+			_shape[i] = dim;
+			Logger::log("dimension: " + to_string(_shape[i]));
+			i++;
+		}
+		allocated = true;
+		calculateSize();
+		Logger::log("size: " + to_string(size));
+		storage = new CellType[size];
+		Logger::logProcess("allocating");
+	}
+	template<typename CellType>
+	inline CellType& Tensor<CellType>::at(std::initializer_list<uint> indices)
+	{
+		Logger::logProcess("at");
+		if (!allocated)
+			error("tensor not yet allocated");
+		if (indices.size() != dims)
+			error(to_string(indices.size())
+				+ " indices given for a tensor with dimensionality of "
+				+ to_string(dims));
+		uint i = 0u;
+		for (const auto index : indices) {
+			if (index >= _shape[i])
+				error("index"
+					+ to_string(index)
+					+ " at dimension "
+					+ to_string(i)
+					+ " is out of range of "
+					+ to_string(_shape[i]));
+			i++;
+		}
+		Logger::logProcess("at");
+		return this->operator()(indices);
+	}
+	template<typename CellType>
+	inline CellType& Tensor<CellType>::operator()(std::initializer_list<uint> indices)
+	{
+		Logger::logProcess("()");
+		uint indexSum = 0u;
+		uint i = 0;
+		for (const auto index : indices) {
+			indexSum *= _shape[i];
+			indexSum += index;
+			i++;
+		}
+		Logger::logProcess("()");
+		return storage[indexSum];
+	}
+	template<typename CellType>
+	inline std::string Tensor<CellType>::toString()
+	{
+		Logger::logProcess("toString");
+		std::string str;
+		for (CellType* i = begin(); i < end(); i++)
+			str += to_string(*i) + " ";
+		Logger::logProcess("toString");
+		return str;
+	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,12 @@ int main(int argc, char** argv) {
     //Model model("cylinder.stl");
 
     Logger::logProcess("testing tensor");
-    tr::Tensor<int> tensor({1,2,3});
+    tr::Tensor<int> tensor({ 1,2,3 }, 0);
+    Logger::log(tensor.toString());
+    int count = 0;
+    for (unsigned int i = 0; i < 2; i++)
+        for (unsigned int x = 0; x < 3; x++)
+            tensor({0, i, x}) = count++;
     Logger::log(tensor.toString());
     Logger::logProcess("testing tensor");
     /*PoseEstimator poseEstimator(frame.cols, frame.rows, model);
